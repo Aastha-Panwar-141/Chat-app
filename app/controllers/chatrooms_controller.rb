11e81d896ddf6b2a79c539d96c0e9ae5e47987f9
Aastha-Panwar-141 <asthap@shriffle.com>
2023-12-11 19:34:17 +0530

    users can send msg to each other in a chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,7 @@
 class ChatroomsController < ApplicationController
   def index
+    @current_user = current_user
+    redirect_to '/users/sign_in' unless @current_user
     @chatrooms = Chatroom.all
     @users = User.all
     @chatroom = Chatroom.new
@@ -7,21 +9,27 @@ class ChatroomsController < ApplicationController
 
   end
   def show
-    byebug
+    # byebug
+    @current_user = current_user
     @single_room = Chatroom.find(params[:id])
-    @chatroom = Chatroom.all
+    @chatrooms = Chatroom.all
     @message = Message.new
+    @chatroom = Chatroom.new
     @messages = @single_room.messages
     # @chatroom = Chatroom.find(params[:id])
+    @users = User.all
     render "index"
   end
+
   def new
     @chatroom = Chatroom.new
   end
+
   def create
     # @chatroom = Chatroom.create(chatroom_params)
     @chatroom = Chatroom.create(name: params["chatroom"]["name"])
     redirect_to @chatroom
+    # redirect_to chatroom_path
   end
   private
   def chatroom_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   get 'chatrooms/new'
   get 'chatrooms/show'
   get 'chatrooms/create'
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
